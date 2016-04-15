0ed09ce3ec0e083f32f7cb781dcf780af30fa0e2
Mc <zhaogw@asiainfo.com>
2016-04-15 20:38:55 +0800

    init

---
+++ user-api/src/main/java/com/ai/slp/user/api/demo/interfaces/IDemoSV.java
@@ -0,0 +1,12 @@
+package com.ai.slp.user.api.demo.interfaces;
+
+/**
+ * demo示例接口,仅用于工程结构说�?,正式环境中请删除<br>
+ *
+ * Date: 2016�?4�?6�? <br>
+ * Copyright (c) 2016 asiainfo.com <br>
+ * @author liutong5
+ */
+public interface IDemoSV {
+
+}
---
+++ user-core/src/main/java/com/ai/slp/user/api/demo/impl/DemoSVImpl.java
@@ -0,0 +1,18 @@
+package com.ai.slp.user.api.demo.impl;
+
+import com.ai.slp.user.api.demo.interfaces.IDemoSV;
+import com.alibaba.dubbo.config.annotation.Service;
+import org.springframework.stereotype.Component;
+
+/**
+ * 接口实现示例,正式环境中,务必上传 <br>
+ *
+ * Date: 2016年3月29日 <br>
+ * Copyright (c) 2016 asiainfo.com <br>
+ * @author liutong5
+ */
+@Service
+@Component
+public class DemoSVImpl implements IDemoSV {
+
+}
