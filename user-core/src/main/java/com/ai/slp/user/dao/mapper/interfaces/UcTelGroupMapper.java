35d0a939aa942a22115fb4250e327103f8d55a3d
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-04-26 19:02:13 +0800

    修改selectByExample的返回类型

--- user-core/src/main/java/com/ai/slp/user/dao/mapper/interfaces/UcTelGroupMapper.java
+++ user-core/src/main/java/com/ai/slp/user/dao/mapper/interfaces/UcTelGroupMapper.java
@@ -1,8 +1,11 @@
 package com.ai.slp.user.dao.mapper.interfaces;
 
+import com.ai.slp.user.api.ucTelGroupInfo.param.UcTelGroupParamsRequest;
 import com.ai.slp.user.dao.mapper.bo.UcTelGroup;
 import com.ai.slp.user.dao.mapper.bo.UcTelGroupCriteria;
 import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Param;
 
 public interface UcTelGroupMapper {
@@ -10,11 +13,11 @@ public interface UcTelGroupMapper {
 
     int deleteByExample(UcTelGroupCriteria example);
 
-    int insert(UcTelGroup record);
+    int insert(UcTelGroupParamsRequest contactsGroup);
 
     int insertSelective(UcTelGroup record);
 
-    List<UcTelGroup> selectByExample(UcTelGroupCriteria example);
+    List<Map<String,Object>> selectByExample(UcTelGroupCriteria example);
 
     int updateByExampleSelective(@Param("record") UcTelGroup record, @Param("example") UcTelGroupCriteria example);
 
