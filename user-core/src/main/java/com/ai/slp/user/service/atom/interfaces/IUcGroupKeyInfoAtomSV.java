9c933003298986482f3c38e73920f771c0e32187
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-04-28 16:10:52 +0800

    客户关键信息

---
+++ user-core/src/main/java/com/ai/slp/user/service/atom/interfaces/IUcGroupKeyInfoAtomSV.java
@@ -0,0 +1,7 @@
+package com.ai.slp.user.service.atom.interfaces;
+
+import com.ai.slp.user.dao.mapper.bo.UcGroupKeyInfo;
+
+public interface IUcGroupKeyInfoAtomSV {
+    public void insertUcGroupKeyInfo(UcGroupKeyInfo ucGroupKey);
+}
