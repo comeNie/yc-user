7ba38df5bcf63be78d5d4f9646e1e0ba19f6a5dd
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-04-28 16:07:31 +0800

    IRegisterSV的实现类

---
+++ user-core/src/main/java/com/ai/slp/user/api/register/impl/RegisterSVImpl.java
@@ -0,0 +1,112 @@
+package com.ai.slp.user.api.register.impl;
+
+import com.ai.opt.base.vo.BaseResponse;
+import com.ai.slp.user.api.register.interfaces.IRegisterSV;
+import com.ai.slp.user.api.register.param.UcBankKeyInfoParams;
+import com.ai.slp.user.api.register.param.UcContactInfoParams;
+import com.ai.slp.user.api.register.param.UcCustKeyInfoParams;
+import com.ai.slp.user.api.register.param.UcGroupKeyInfoParams;
+import com.ai.slp.user.api.register.param.UcUserAgreeParams;
+import com.ai.slp.user.api.register.param.UcUserFileExtParams;
+import com.ai.slp.user.api.register.param.UcUserParams;
+
+public class RegisterSVImpl implements IRegisterSV {
+
+    @Override
+    public BaseResponse insertUcUser(UcUserParams ucUser, UcUserAgreeParams agreeInfo,
+            UcContactInfoParams contactInfo) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse updateUserInfo(UcUserParams ucUser, UcUserAgreeParams agreeInfo,
+            UcContactInfoParams contactInfo, UcUserFileExtParams userFileExt) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse searchUserInfo(UcUserParams ucUser) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse searchUserIList(UcUserParams ucUser) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse insertUcCustInfo(UcCustKeyInfoParams ucCust) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse searchUcCustInfo(UcCustKeyInfoParams ucCust) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse updateUcCustInfo(UcCustKeyInfoParams ucCust) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse insertUcGroupInfo(UcGroupKeyInfoParams ucGroup) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse searchUcGroupInfo(UcGroupKeyInfoParams ucGroup) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse updateUcGroupInfo(UcGroupKeyInfoParams ucGroup) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse insertUcContactInfo(UcContactInfoParams ucContact) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse searchUcContactInfo(UcContactInfoParams ucContact) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse updateUcContactInfo(UcContactInfoParams ucContact) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse insertUcBankInfo(UcBankKeyInfoParams ucBank) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse searchUcBankInfo(UcBankKeyInfoParams ucBank) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse updateUcBankInfo(UcBankKeyInfoParams ucBank) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse insertUcUserFileExtInfo(UcUserFileExtParams ucUserFileExt) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse searchUcUserFileExtInfo(UcUserFileExtParams ucUserFileExt) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse updateUcUserFileExtInfo(UcUserFileExtParams ucUserFileExt) {
+        return null;
+    }
+
+}
