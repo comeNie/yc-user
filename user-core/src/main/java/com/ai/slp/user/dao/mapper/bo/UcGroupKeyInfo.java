099e1d9e9f05853dc1883272823595ba3e40c610
Zh <Zh@10.1.64.58>
2016-04-28 14:20:45 +0800

    添加 uc_group_key_info

---
+++ user-core/src/main/java/com/ai/slp/user/dao/mapper/bo/UcGroupKeyInfo.java
@@ -0,0 +1,345 @@
+package com.ai.slp.user.dao.mapper.bo;
+
+import java.sql.Timestamp;
+
+public class UcGroupKeyInfo {
+    private String tenantId;
+
+    private Long userId;
+
+    private String userType;
+
+    private String certNum;
+
+    private String custName;
+
+    private String provinceCode;
+
+    private String cityCode;
+
+    private String countyCode;
+
+    private String certAddr;
+
+    private Timestamp certIssueDate;
+
+    private Long registeredCapitass;
+
+    private Timestamp certValidDate;
+
+    private Timestamp certInvalidDate;
+
+    private String webFlag;
+
+    private String groupWebsite;
+
+    private String groupMemberScale;
+
+    private String groupType;
+
+    private String groupIndustry;
+
+    private String groupBusinessScope;
+
+    private String groupProduct;
+
+    private String legalPerson;
+
+    private String legalCertNum;
+
+    private String orgCode;
+
+    private String taxpayerCode;
+
+    private String taxpayerType;
+
+    private String taxpayerTypeCode;
+
+    private String groupInfo;
+
+    private Timestamp createTime;
+
+    private String createChlId;
+
+    private Long createOperId;
+
+    private Timestamp updateTime;
+
+    private String updateChlId;
+
+    private Long updateOperId;
+
+    private String contractId;
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId == null ? null : tenantId.trim();
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType == null ? null : userType.trim();
+    }
+
+    public String getCertNum() {
+        return certNum;
+    }
+
+    public void setCertNum(String certNum) {
+        this.certNum = certNum == null ? null : certNum.trim();
+    }
+
+    public String getCustName() {
+        return custName;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName == null ? null : custName.trim();
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode == null ? null : provinceCode.trim();
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode == null ? null : cityCode.trim();
+    }
+
+    public String getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(String countyCode) {
+        this.countyCode = countyCode == null ? null : countyCode.trim();
+    }
+
+    public String getCertAddr() {
+        return certAddr;
+    }
+
+    public void setCertAddr(String certAddr) {
+        this.certAddr = certAddr == null ? null : certAddr.trim();
+    }
+
+    public Timestamp getCertIssueDate() {
+        return certIssueDate;
+    }
+
+    public void setCertIssueDate(Timestamp certIssueDate) {
+        this.certIssueDate = certIssueDate;
+    }
+
+    public Long getRegisteredCapitass() {
+        return registeredCapitass;
+    }
+
+    public void setRegisteredCapitass(Long registeredCapitass) {
+        this.registeredCapitass = registeredCapitass;
+    }
+
+    public Timestamp getCertValidDate() {
+        return certValidDate;
+    }
+
+    public void setCertValidDate(Timestamp certValidDate) {
+        this.certValidDate = certValidDate;
+    }
+
+    public Timestamp getCertInvalidDate() {
+        return certInvalidDate;
+    }
+
+    public void setCertInvalidDate(Timestamp certInvalidDate) {
+        this.certInvalidDate = certInvalidDate;
+    }
+
+    public String getWebFlag() {
+        return webFlag;
+    }
+
+    public void setWebFlag(String webFlag) {
+        this.webFlag = webFlag == null ? null : webFlag.trim();
+    }
+
+    public String getGroupWebsite() {
+        return groupWebsite;
+    }
+
+    public void setGroupWebsite(String groupWebsite) {
+        this.groupWebsite = groupWebsite == null ? null : groupWebsite.trim();
+    }
+
+    public String getGroupMemberScale() {
+        return groupMemberScale;
+    }
+
+    public void setGroupMemberScale(String groupMemberScale) {
+        this.groupMemberScale = groupMemberScale == null ? null : groupMemberScale.trim();
+    }
+
+    public String getGroupType() {
+        return groupType;
+    }
+
+    public void setGroupType(String groupType) {
+        this.groupType = groupType == null ? null : groupType.trim();
+    }
+
+    public String getGroupIndustry() {
+        return groupIndustry;
+    }
+
+    public void setGroupIndustry(String groupIndustry) {
+        this.groupIndustry = groupIndustry == null ? null : groupIndustry.trim();
+    }
+
+    public String getGroupBusinessScope() {
+        return groupBusinessScope;
+    }
+
+    public void setGroupBusinessScope(String groupBusinessScope) {
+        this.groupBusinessScope = groupBusinessScope == null ? null : groupBusinessScope.trim();
+    }
+
+    public String getGroupProduct() {
+        return groupProduct;
+    }
+
+    public void setGroupProduct(String groupProduct) {
+        this.groupProduct = groupProduct == null ? null : groupProduct.trim();
+    }
+
+    public String getLegalPerson() {
+        return legalPerson;
+    }
+
+    public void setLegalPerson(String legalPerson) {
+        this.legalPerson = legalPerson == null ? null : legalPerson.trim();
+    }
+
+    public String getLegalCertNum() {
+        return legalCertNum;
+    }
+
+    public void setLegalCertNum(String legalCertNum) {
+        this.legalCertNum = legalCertNum == null ? null : legalCertNum.trim();
+    }
+
+    public String getOrgCode() {
+        return orgCode;
+    }
+
+    public void setOrgCode(String orgCode) {
+        this.orgCode = orgCode == null ? null : orgCode.trim();
+    }
+
+    public String getTaxpayerCode() {
+        return taxpayerCode;
+    }
+
+    public void setTaxpayerCode(String taxpayerCode) {
+        this.taxpayerCode = taxpayerCode == null ? null : taxpayerCode.trim();
+    }
+
+    public String getTaxpayerType() {
+        return taxpayerType;
+    }
+
+    public void setTaxpayerType(String taxpayerType) {
+        this.taxpayerType = taxpayerType == null ? null : taxpayerType.trim();
+    }
+
+    public String getTaxpayerTypeCode() {
+        return taxpayerTypeCode;
+    }
+
+    public void setTaxpayerTypeCode(String taxpayerTypeCode) {
+        this.taxpayerTypeCode = taxpayerTypeCode == null ? null : taxpayerTypeCode.trim();
+    }
+
+    public String getGroupInfo() {
+        return groupInfo;
+    }
+
+    public void setGroupInfo(String groupInfo) {
+        this.groupInfo = groupInfo == null ? null : groupInfo.trim();
+    }
+
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateChlId() {
+        return createChlId;
+    }
+
+    public void setCreateChlId(String createChlId) {
+        this.createChlId = createChlId == null ? null : createChlId.trim();
+    }
+
+    public Long getCreateOperId() {
+        return createOperId;
+    }
+
+    public void setCreateOperId(Long createOperId) {
+        this.createOperId = createOperId;
+    }
+
+    public Timestamp getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Timestamp updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getUpdateChlId() {
+        return updateChlId;
+    }
+
+    public void setUpdateChlId(String updateChlId) {
+        this.updateChlId = updateChlId == null ? null : updateChlId.trim();
+    }
+
+    public Long getUpdateOperId() {
+        return updateOperId;
+    }
+
+    public void setUpdateOperId(Long updateOperId) {
+        this.updateOperId = updateOperId;
+    }
+
+    public String getContractId() {
+        return contractId;
+    }
+
+    public void setContractId(String contractId) {
+        this.contractId = contractId == null ? null : contractId.trim();
+    }
+}
