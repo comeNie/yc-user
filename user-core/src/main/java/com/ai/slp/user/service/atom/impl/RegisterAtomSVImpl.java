7f2d08a1e91dca4a3fc7cde342995fd103295f18
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-04-28 18:33:45 +0800

    用户注册

--- user-core/src/main/java/com/ai/slp/user/service/atom/impl/RegisterAtomSVImpl.java
+++ user-core/src/main/java/com/ai/slp/user/service/atom/impl/RegisterAtomSVImpl.java
@@ -2,7 +2,15 @@ package com.ai.slp.user.service.atom.impl;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.ai.slp.user.dao.mapper.bo.UcContactsInfo;
+import com.ai.slp.user.dao.mapper.bo.UcGroupKeyInfo;
+import com.ai.slp.user.dao.mapper.bo.UcGroupKeyInfoCriteria;
+import com.ai.slp.user.dao.mapper.bo.UcStateChg;
+import com.ai.slp.user.dao.mapper.bo.UcStateChgCriteria;
 import com.ai.slp.user.dao.mapper.bo.UcUser;
+import com.ai.slp.user.dao.mapper.bo.UcUserAgree;
 import com.ai.slp.user.dao.mapper.bo.UcUserCriteria;
 import com.ai.slp.user.dao.mapper.factory.MapperFactory;
 import com.ai.slp.user.service.atom.interfaces.IRegisterAtomSV;
@@ -19,5 +27,37 @@ public class RegisterAtomSVImpl implements IRegisterAtomSV {
         return MapperFactory.getUcUserMapper().selectByExample(criteria);
     }
 
+    @Override
+    public int insertUcGroupKeyInfo(UcGroupKeyInfo ucGroupKey) {
+        return MapperFactory.getUcGroupKeyInfoMapper().insert(ucGroupKey);
+    }
+
+    @Override
+    public List<UcGroupKeyInfo> getUcGroupKeyInfo(UcGroupKeyInfoCriteria criteria) {
+        return MapperFactory.getUcGroupKeyInfoMapper().selectByExample(criteria);
+    }
+
+    @Override
+    public int insertUcContactsInfo(UcContactsInfo ucContactsInfo) {
+        
+        return MapperFactory.getUcContactsInfoMapper().insert(ucContactsInfo);
+    }
+
+    @Override
+    public void InsertUcUserAgreeAtomSv(UcUserAgree record) {
+         MapperFactory.getUcUserAgreeMapper().insert(record);
+    }
+
+    @Override
+    public int updateUcStateChgBusiInfo(@Param("record")
+    UcStateChg record, @Param("example")
+    UcStateChgCriteria example) {
+        return MapperFactory.getUcStateChgMapper().updateByExample(record, example);
+    }
+
+    @Override
+    public int insertUcStateChgBusiInfo(UcStateChg ucStateChgParam) {
+        return MapperFactory.getUcStateChgMapper().insert(ucStateChgParam);
+    }
     
 }
