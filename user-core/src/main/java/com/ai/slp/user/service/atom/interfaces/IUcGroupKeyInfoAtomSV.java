5713d2602b7608d3d7e955cfcb4555a3bdd0b2fa
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-04-29 15:51:34 +0800

    删除掉UcGroupkeyinfo相关接口

--- user-core/src/main/java/com/ai/slp/user/service/atom/interfaces/IUcGroupKeyInfoAtomSV.java
+++
@@ -1,7 +0,0 @@
-package com.ai.slp.user.service.atom.interfaces;
-
-import com.ai.slp.user.dao.mapper.bo.UcGroupKeyInfo;
-
-public interface IUcGroupKeyInfoAtomSV {
-    public void insertUcGroupKeyInfo(UcGroupKeyInfo ucGroupKey);
-}
