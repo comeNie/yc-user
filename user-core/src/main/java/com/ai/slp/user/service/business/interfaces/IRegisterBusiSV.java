3cc5286c4e5dd9012842593cb52b33702a6cc7e2
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-04-29 15:49:59 +0800

    用户注册

--- user-core/src/main/java/com/ai/slp/user/service/business/interfaces/IRegisterBusiSV.java
+++ user-core/src/main/java/com/ai/slp/user/service/business/interfaces/IRegisterBusiSV.java
@@ -2,6 +2,8 @@ package com.ai.slp.user.service.business.interfaces;
 
 
 import com.ai.opt.base.vo.BaseResponse;
+import com.ai.slp.user.api.register.param.UcBankKeyInfoParams;
+import com.ai.slp.user.api.register.param.UcContactInfoParams;
 import com.ai.slp.user.api.register.param.UcGroupKeyInfoParams;
 import com.ai.slp.user.api.register.param.UcUserParams;
 
@@ -9,7 +11,11 @@ public interface IRegisterBusiSV {
     
     public BaseResponse insertUserInfo(UcUserParams userParams);
     
-    public BaseResponse insertUcGroupKeyInfo(UcGroupKeyInfoParams ucGroupKeyInfoParams);
+    public BaseResponse insertCompanyInfoAttest(UcUserParams userParams,UcGroupKeyInfoParams ucGroupKeyInfoParams,UcContactInfoParams ucContactInfoParams);
     
     public BaseResponse getUserInfo(UcUserParams userParams);
+    
+    public BaseResponse getUcGroupKeyInfo(UcGroupKeyInfoParams ucGroupKeyInfoParams);
+    
+    public BaseResponse insertAgentInfoAttest(UcUserParams userParams,UcGroupKeyInfoParams ucGroupKeyInfoParams,UcContactInfoParams ucContactInfoParams,UcBankKeyInfoParams ucBankKeyInfoParam);
 }
