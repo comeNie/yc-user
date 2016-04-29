51283a2590ceff5c4655dfb8a7a1713f96201c63
Zh <Zh@10.1.64.55>
2016-04-29 17:08:12 +0800

    add

--- user-core/src/main/java/com/ai/slp/user/service/atom/impl/RegisterAtomSVImpl.java
+++ user-core/src/main/java/com/ai/slp/user/service/atom/impl/RegisterAtomSVImpl.java
@@ -8,12 +8,14 @@ import org.springframework.stereotype.Component;
 import com.ai.slp.user.dao.mapper.bo.UcBankInfo;
 import com.ai.slp.user.dao.mapper.bo.UcContactsInfo;
 import com.ai.slp.user.dao.mapper.bo.UcCustKeyInfo;
+import com.ai.slp.user.dao.mapper.bo.UcCustKeyInfoCriteria;
 import com.ai.slp.user.dao.mapper.bo.UcGroupKeyInfo;
 import com.ai.slp.user.dao.mapper.bo.UcGroupKeyInfoCriteria;
 import com.ai.slp.user.dao.mapper.bo.UcStateChg;
 import com.ai.slp.user.dao.mapper.bo.UcStateChgCriteria;
 import com.ai.slp.user.dao.mapper.bo.UcUser;
 import com.ai.slp.user.dao.mapper.bo.UcUserAgree;
+import com.ai.slp.user.dao.mapper.bo.UcUserAgreeCriteria;
 import com.ai.slp.user.dao.mapper.bo.UcUserCriteria;
 import com.ai.slp.user.dao.mapper.factory.MapperFactory;
 import com.ai.slp.user.service.atom.interfaces.IRegisterAtomSV;
@@ -72,6 +74,26 @@ public class RegisterAtomSVImpl implements IRegisterAtomSV {
     @Override
     public int insertUcCustKeyInfo(UcCustKeyInfo ucContactsInfo) {
         return MapperFactory.getUcCustKeyInfoMapper().insert(ucContactsInfo);
+    }
+
+    @Override
+    public int updateUserInfo(UcUser record, UcUserCriteria example) {
+        return MapperFactory.getUcUserMapper().updateByExample(record, example);
+    }
+
+    @Override
+    public int updateGroupKeyInfo(UcGroupKeyInfo record, UcGroupKeyInfoCriteria example) {
+        return MapperFactory.getUcGroupKeyInfoMapper().updateByExampleSelective(record, example);
+    }
+
+    @Override
+    public int updateUserAgree(UcUserAgree record, UcUserAgreeCriteria example) {
+        return MapperFactory.getUcUserAgreeMapper().updateByExample(record, example);
+    }
+
+    @Override
+    public int updateCustKeyInfo(UcCustKeyInfo record, UcCustKeyInfoCriteria example) {
+        return MapperFactory.getUcCustKeyInfoMapper().updateByExample(record, example);
     }
     
 }
--- user-core/src/main/java/com/ai/slp/user/service/atom/interfaces/IRegisterAtomSV.java
+++ user-core/src/main/java/com/ai/slp/user/service/atom/interfaces/IRegisterAtomSV.java
@@ -7,38 +7,58 @@ import org.apache.ibatis.annotations.Param;
 import com.ai.slp.user.dao.mapper.bo.UcBankInfo;
 import com.ai.slp.user.dao.mapper.bo.UcContactsInfo;
 import com.ai.slp.user.dao.mapper.bo.UcCustKeyInfo;
+import com.ai.slp.user.dao.mapper.bo.UcCustKeyInfoCriteria;
 import com.ai.slp.user.dao.mapper.bo.UcGroupKeyInfo;
 import com.ai.slp.user.dao.mapper.bo.UcGroupKeyInfoCriteria;
 import com.ai.slp.user.dao.mapper.bo.UcStateChg;
 import com.ai.slp.user.dao.mapper.bo.UcStateChgCriteria;
 import com.ai.slp.user.dao.mapper.bo.UcUser;
 import com.ai.slp.user.dao.mapper.bo.UcUserAgree;
+import com.ai.slp.user.dao.mapper.bo.UcUserAgreeCriteria;
 import com.ai.slp.user.dao.mapper.bo.UcUserCriteria;
 
 public interface IRegisterAtomSV {
-    //用户
+    // 用户
     public int insertUserInfo(UcUser user);
+
     public List<UcUser> getUserInfo(UcUserCriteria criteria);
-    
-    //企业认证   
+
+    int updateUserInfo(@Param("record") UcUser record,
+            @Param("example") UcUserCriteria example);
+
+    // 企业认证
     public int insertUcGroupKeyInfo(UcGroupKeyInfo ucGroupKey);
+
     public List<UcGroupKeyInfo> getUcGroupKeyInfo(UcGroupKeyInfoCriteria criteria);
     
-    //用户联系人
+    int updateGroupKeyInfo(@Param("record") UcGroupKeyInfo record,
+            @Param("example") UcGroupKeyInfoCriteria example);
+
+
+    // 用户联系人
     public int insertUcContactsInfo(UcContactsInfo ucContactsInfo);
-    
-    //用户协议
+
+    // 用户协议
     public void InsertUcUserAgreeAtomSv(UcUserAgree record);
     
-    //用户状态轨迹
+    int updateUserAgree(@Param("record") UcUserAgree record,
+            @Param("example") UcUserAgreeCriteria example);
+
+
+    // 用户状态轨迹
     public int insertUcStateChgBusiInfo(UcStateChg ucStateChgParam);
-    public int updateUcStateChgBusiInfo(@Param("record")UcStateChg record, @Param("example")
-    UcStateChgCriteria example);
-    
-    //用户银行信息
+
+    public int updateUcStateChgBusiInfo(@Param("record") UcStateChg record,
+            @Param("example") UcStateChgCriteria example);
+
+    // 用户银行信息
     public int insertUcBankInfo(UcBankInfo ucBankInfo);
-    
-    //个人客户关系信息
+
+    // 个人客户关系信息
     public int insertUcCustKeyInfo(UcCustKeyInfo ucContactsInfo);
     
+    int updateCustKeyInfo(@Param("record") UcCustKeyInfo record,
+            @Param("example") UcCustKeyInfoCriteria example);
+
+
 }
