c325a4d75258b02e6ab9367d0919f0717b506c47
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-05-12 20:10:23 +0800

    修改接口规范

--- user-core/src/main/java/com/ai/slp/user/api/register/impl/RegisterSVImpl.java
+++ user-core/src/main/java/com/ai/slp/user/api/register/impl/RegisterSVImpl.java
@@ -1,37 +1,129 @@
 package com.ai.slp.user.api.register.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import com.ai.opt.base.vo.BaseResponse;
+import com.ai.opt.base.vo.ResponseHeader;
 import com.ai.slp.user.api.register.interfaces.IRegisterSV;
+import com.ai.slp.user.api.register.param.RegisterParamsRequest;
+import com.ai.slp.user.api.register.param.RegisterParamsResponse;
+import com.ai.slp.user.api.register.param.UcBankKeyInfoParams;
 import com.ai.slp.user.api.register.param.UcContactInfoParams;
-import com.ai.slp.user.api.register.param.UcUserAgreeParams;
+import com.ai.slp.user.api.register.param.UcCustKeyInfoParams;
+import com.ai.slp.user.api.register.param.UcGroupKeyInfoParams;
+import com.ai.slp.user.api.register.param.UcUserFileExtParams;
 import com.ai.slp.user.api.register.param.UcUserParams;
-import com.ai.slp.user.api.register.param.UserParams;
-import com.ai.slp.user.api.register.param.UserResponse;
+import com.ai.slp.user.constants.ExceptCodeConstants;
 import com.ai.slp.user.service.business.interfaces.IRegisterBusiSV;
+import com.alibaba.dubbo.config.annotation.Service;
 
-@Service
+@Service(validation = "true")
 public class RegisterSVImpl implements IRegisterSV {
 
     @Autowired
     public IRegisterBusiSV registerBusiSv;
     
     @Override
-    public BaseResponse insertUcUser(UcUserParams ucUser, UcUserAgreeParams agreeInfo,
-            UcContactInfoParams contactInfo) {
-        return registerBusiSv.insertUserInfo(ucUser,agreeInfo,contactInfo);
+    public RegisterParamsResponse insertUcUser(RegisterParamsRequest registerParamsRequest) {
+        registerBusiSv.insertUserInfo(registerParamsRequest);
+        ResponseHeader responseHeader = new ResponseHeader(true, ExceptCodeConstants.SUCCESS, "成功");
+        RegisterParamsResponse result = new RegisterParamsResponse();
+        result.setResponseHeader(responseHeader);
+        return result;
+    }
+
+  /*  @Override
+    public RegisterParamsResponse updateUserInfo(UpdateUserParams updateUserParams) {
+        registerBusiSv.updateUserInfo(updateUserParams);
+        ResponseHeader responseHeader = new ResponseHeader(true, ExceptCodeConstants.SUCCESS, "成功");
+        RegisterParamsResponse result = new RegisterParamsResponse();
+        result.setResponseHeader(responseHeader);
+        return result;
+    }*/
+
+    @Override
+    public RegisterParamsResponse searchUserInfo(UcUserParams ucUser) {
+        return null;
     }
 
     @Override
-    public BaseResponse updateUserInfo(UserParams updateUserParams) {
-        return registerBusiSv.updateUserInfo(updateUserParams);
+    public BaseResponse searchUserIList(UcUserParams ucUser) {
+        return null;
     }
 
     @Override
-    public UserResponse searchUserInfo(UcUserParams ucUser) {
-        return registerBusiSv.searchUserInfo(ucUser);
+    public BaseResponse insertUcCustInfo(UcCustKeyInfoParams ucCust) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse searchUcCustInfo(UcCustKeyInfoParams ucCust) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse updateUcCustInfo(UcCustKeyInfoParams ucCust) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse insertUcGroupInfo(UcGroupKeyInfoParams ucGroup) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse searchUcGroupInfo(UcGroupKeyInfoParams ucGroup) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse updateUcGroupInfo(UcGroupKeyInfoParams ucGroup) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse insertUcContactInfo(UcContactInfoParams ucContact) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse searchUcContactInfo(UcContactInfoParams ucContact) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse updateUcContactInfo(UcContactInfoParams ucContact) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse insertUcBankInfo(UcBankKeyInfoParams ucBank) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse searchUcBankInfo(UcBankKeyInfoParams ucBank) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse updateUcBankInfo(UcBankKeyInfoParams ucBank) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse insertUcUserFileExtInfo(UcUserFileExtParams ucUserFileExt) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse searchUcUserFileExtInfo(UcUserFileExtParams ucUserFileExt) {
+        return null;
+    }
+
+    @Override
+    public BaseResponse updateUcUserFileExtInfo(UcUserFileExtParams ucUserFileExt) {
+        return null;
     }
 
 }
