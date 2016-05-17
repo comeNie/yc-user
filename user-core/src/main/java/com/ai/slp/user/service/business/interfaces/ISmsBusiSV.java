3747ae9fbd0d04481fcb3575c3f2a1d6b50d51c6
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-05-17 11:41:24 +0800

    手机验证码功能

---
+++ user-core/src/main/java/com/ai/slp/user/service/business/interfaces/ISmsBusiSV.java
@@ -0,0 +1,12 @@
+package com.ai.slp.user.service.business.interfaces;
+
+import com.ai.slp.user.vo.SMSInputVo;
+
+
+public interface ISmsBusiSV {
+
+	String dataInput(String param);
+
+	//String dataInput(SMSInputVo sMSInputVo);
+
+}
