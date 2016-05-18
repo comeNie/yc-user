dd8126a84a7ddf5d9052365f2e58a5e01ab49032
Zh <Zh@10.1.64.26>
2016-05-18 21:11:55 +0800

    修改service注解

--- user-core/src/main/java/com/ai/slp/user/api/login/impl/LoginSVImpl.java
+++ user-core/src/main/java/com/ai/slp/user/api/login/impl/LoginSVImpl.java
@@ -1,7 +1,6 @@
 package com.ai.slp.user.api.login.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
@@ -10,6 +9,7 @@ import com.ai.slp.user.api.login.interfaces.ILoginSV;
 import com.ai.slp.user.api.login.param.LoginRequest;
 import com.ai.slp.user.api.login.param.LoginResponse;
 import com.ai.slp.user.service.business.interfaces.ILoginBusiSV;
+import com.alibaba.dubbo.config.annotation.Service;
 
 /**
  * 登录服务实现 Date: 2016年4月22日 <br>
@@ -17,8 +17,8 @@ import com.ai.slp.user.service.business.interfaces.ILoginBusiSV;
  * 
  * @author zhangqiang7
  */
-//@Service(validation = "true")
-@Service
+@Service(validation = "true")
+//@Service
 public class LoginSVImpl implements ILoginSV {
 
     @Autowired
