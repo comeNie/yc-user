0f17272c80065d5088842234e5937a2e97cbe4d9
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-05-24 15:09:58 +0800

    查询用户返回的属性

---
+++ user-api/src/main/java/com/ai/slp/user/api/ucuser/param/SearchUserResponse.java
@@ -0,0 +1,27 @@
+package com.ai.slp.user.api.ucuser.param;
+
+import com.ai.opt.base.vo.BaseInfo;
+import com.ai.opt.base.vo.BaseResponse;
+import com.ai.opt.base.vo.ResponseHeader;
+
+public class SearchUserResponse  extends BaseResponse{
+    private static final long serialVersionUID = 1L;
+    public String userMp;
+    public String userEmail;
+    public String getUserMp() {
+        return userMp;
+    }
+    public void setUserMp(String userMp) {
+        this.userMp = userMp;
+    }
+    public String getUserEmail() {
+        return userEmail;
+    }
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+    public void setResponseHeader(ResponseHeader responseHeader) {
+    }
+    
+    
+}
