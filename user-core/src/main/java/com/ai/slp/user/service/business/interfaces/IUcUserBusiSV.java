2391887f0bc80b6dc4d16d1150ae7f09f4e162ae
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-05-24 17:39:50 +0800

    添加接口updateByAccountId

--- user-core/src/main/java/com/ai/slp/user/service/business/interfaces/IUcUserBusiSV.java
+++ user-core/src/main/java/com/ai/slp/user/service/business/interfaces/IUcUserBusiSV.java
@@ -4,6 +4,7 @@ import com.ai.opt.base.exception.SystemException;
 import com.ai.slp.user.api.ucuser.param.SearchUserRequest;
 import com.ai.slp.user.api.ucuser.param.SearchUserListResponse;
 import com.ai.slp.user.dao.mapper.bo.UcUser;
+import com.ai.slp.user.dao.mapper.bo.UcUserCriteria;
 
 /**
  * 用户信息 Date: 2016年4月29日 <br>
@@ -19,4 +20,6 @@ public interface IUcUserBusiSV {
     UcUser queryByEmail(String email)throws SystemException;
     
     UcUser queryBaseInfo(String userId) throws SystemException;
+    
+    int updateByAccountId(UcUser gnAccount,UcUserCriteria example) throws SystemException;
 }
