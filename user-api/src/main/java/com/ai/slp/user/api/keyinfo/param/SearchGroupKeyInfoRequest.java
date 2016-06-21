33fb147d9827212cd3d065890b92542f7f64c311
Zh <Zh@10.1.64.77>
2016-06-21 16:29:10 +0800

    去除非空校验

--- user-api/src/main/java/com/ai/slp/user/api/keyinfo/param/SearchGroupKeyInfoRequest.java
+++ user-api/src/main/java/com/ai/slp/user/api/keyinfo/param/SearchGroupKeyInfoRequest.java
@@ -1,15 +1,11 @@
 package com.ai.slp.user.api.keyinfo.param;
 
-import javax.validation.constraints.NotNull;
-
 import com.ai.opt.base.vo.BaseInfo;
-import com.ai.slp.user.api.keyinfo.interfaces.IUcKeyInfoSV;
 
 public class SearchGroupKeyInfoRequest extends BaseInfo {
 
     private static final long serialVersionUID = 1L;
 
-    @NotNull(message = "公司名不能为空", groups = { IUcKeyInfoSV.SearchGroupKeyInfo.class })
     private String custName;
 
     private String userId;
