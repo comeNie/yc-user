5d13b8ea5a64ea0770836b4f76159359940a77e4
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-06-22 16:15:36 +0800

    添加注释

--- user-api/src/main/java/com/ai/slp/user/api/keyinfo/param/SearchGroupKeyInfoRequest.java
+++ user-api/src/main/java/com/ai/slp/user/api/keyinfo/param/SearchGroupKeyInfoRequest.java
@@ -5,9 +5,13 @@ import com.ai.opt.base.vo.BaseInfo;
 public class SearchGroupKeyInfoRequest extends BaseInfo {
 
     private static final long serialVersionUID = 1L;
-
+    /**
+     * 企业名称
+     */
     private String custName;
-
+    /**
+     * 用户ID
+     */
     private String userId;
 
     public String getCustName() {
