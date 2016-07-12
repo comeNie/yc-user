3a2d483f1408edd7753262b42572fc48c7346669
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-07-12 19:16:25 +0800

    新增加selectCountGroupKeyInfo

--- user-core/src/main/java/com/ai/slp/user/dao/mapper/attach/UcGroupKeyInfoAttrAttachMapper.java
+++ user-core/src/main/java/com/ai/slp/user/dao/mapper/attach/UcGroupKeyInfoAttrAttachMapper.java
@@ -127,4 +127,7 @@ public interface UcGroupKeyInfoAttrAttachMapper {
             "from uc_group_key_info groupInfo,uc_user userInfo where groupInfo.user_id = userInfo.user_id and groupInfo.cust_name like #{custName} and  groupInfo.tenant_id=#{tenantId} limit #{startPage},#{endPage}")
     List<SearchGroupUserInfoResponse> selectGroupKeyInfo(@Param("tenantId")String tenantId,@Param("custName")String custName,@Param("startPage")int startPage,@Param("endPage")int endPage);
     
+    @Select("select count(groupInfo.user_id) from uc_group_key_info groupInfo,uc_user userInfo  " +
+            " where groupInfo.user_id = userInfo.user_id and groupInfo.cust_name like #{custName} and  groupInfo.tenant_id=#{tenantId}")
+    int selectCountGroupKeyInfo(@Param("tenantId")String tenantId,@Param("custName")String custName);
 }
