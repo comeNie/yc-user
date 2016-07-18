4b45964e1e2be22ce4fa29c591ce17cfb276ecb2
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-07-18 11:16:48 +0800

    修改QueryGroupInfo的返回类型

--- user-core/src/main/java/com/ai/slp/user/service/business/interfaces/IUcGroupKeyInfoBusiSV.java
+++ user-core/src/main/java/com/ai/slp/user/service/business/interfaces/IUcGroupKeyInfoBusiSV.java
@@ -2,15 +2,16 @@ package com.ai.slp.user.service.business.interfaces;
 
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
+import com.ai.opt.base.vo.PageInfoResponse;
 import com.ai.slp.user.api.keyinfo.param.InsertCustFileExtRequest;
 import com.ai.slp.user.api.keyinfo.param.InsertGroupKeyInfoRequest;
 import com.ai.slp.user.api.keyinfo.param.QueryCustFileExtRequest;
 import com.ai.slp.user.api.keyinfo.param.QueryCustFileExtResponse;
 import com.ai.slp.user.api.keyinfo.param.QueryGroupInfoRequest;
-import com.ai.slp.user.api.keyinfo.param.QueryGroupInfoResponse;
 import com.ai.slp.user.api.keyinfo.param.SearchGroupKeyInfoRequest;
 import com.ai.slp.user.api.keyinfo.param.SearchGroupKeyInfoResponse;
 import com.ai.slp.user.api.keyinfo.param.SearchGroupUserInfoResponse;
+import com.ai.slp.user.api.keyinfo.param.UcGroupKeyInfoVo;
 import com.ai.slp.user.api.keyinfo.param.UpdateCustFileExtRequest;
 import com.ai.slp.user.api.keyinfo.param.UpdateGroupKeyInfoRequest;
 
@@ -31,7 +32,7 @@ public interface IUcGroupKeyInfoBusiSV {
     QueryCustFileExtResponse QueryCustFileExt(QueryCustFileExtRequest request)
             throws SystemException, BusinessException;
 
-    public QueryGroupInfoResponse QueryGroupInfo(QueryGroupInfoRequest request)
+    public PageInfoResponse<UcGroupKeyInfoVo> QueryGroupInfo(QueryGroupInfoRequest request)
             throws SystemException, BusinessException;
 
     public void updateCustFileExt(UpdateCustFileExtRequest request)
