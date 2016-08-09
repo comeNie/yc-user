9589dc67a380885ea5f0f1d3a8e9ef8394314498
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2016-08-09 15:56:23 +0800

    去掉与slp相关的类

--- user-core/src/main/java/com/ai/slp/user/constants/UserRegisterErrorCode.java
+++
@@ -1,8 +0,0 @@
-package com.ai.slp.user.constants;
-
-public class UserRegisterErrorCode {
-    /**
-     * 用户名或者手机号已注册
-     */
-    public static final String NOTONE_ERROR="10003";
-}
--- user-core/src/main/java/com/ai/slp/user/service/atom/interfaces/IUcUserAgreeAtomSV.java
+++
@@ -1,7 +0,0 @@
-package com.ai.slp.user.service.atom.interfaces;
-
-import com.ai.slp.user.dao.mapper.bo.UcUserAgree;
-
-public interface IUcUserAgreeAtomSV {
-    public void InsertUcUserAgreeAtomSv(UcUserAgree record);
-}
