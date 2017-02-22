f5300fcab2bef4507a7b7d8cc02f911ec4acbbbc
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2017-02-22 10:22:26 +0800

    收藏

---
+++ user-core/src/main/java/com/ai/yc/user/service/atom/impl/YCUserCollectionAtomSVImpl.java
@@ -0,0 +1,40 @@
+package com.ai.yc.user.service.atom.impl;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.ai.yc.user.dao.mapper.bo.UsrCollectionTranslation;
+import com.ai.yc.user.dao.mapper.bo.UsrCollectionTranslationCriteria;
+import com.ai.yc.user.dao.mapper.interfaces.UsrCollectionTranslationMapper;
+import com.ai.yc.user.service.atom.interfaces.IYCUserCollectionAtomSV;
+
+@Component
+public class YCUserCollectionAtomSVImpl implements IYCUserCollectionAtomSV{
+
+	@Autowired
+	private transient UsrCollectionTranslationMapper uCollectionMapper;
+
+	@Override
+	public int insertCollectionInfo(UsrCollectionTranslation collectionInfo) {
+		return uCollectionMapper.insertSelective(collectionInfo);
+	}
+
+	@Override
+	public int updateCollectionInfo(UsrCollectionTranslation collectionInfo) {
+		return uCollectionMapper.updateByPrimaryKeySelective(collectionInfo);
+	}
+
+	@Override
+	public int deleteCollectionInfo(UsrCollectionTranslationCriteria example) {
+		return uCollectionMapper.deleteByExample(example);
+	}
+
+	@Override
+	public List<UsrCollectionTranslation> queryCollectionInfo(
+			UsrCollectionTranslationCriteria example) {
+		return uCollectionMapper.selectByExample(example);
+	}
+}
