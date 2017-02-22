7855c02b9682ed54c9a8d3c66d91136011041700
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2017-02-22 10:18:15 +0800

    收藏和联系人接口

---
+++ user-core/src/main/java/com/ai/yc/user/api/usercollection/impl/YCUserCollectionSVImpl.java
@@ -0,0 +1,64 @@
+package com.ai.yc.user.api.usercollection.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.ai.opt.base.vo.BaseResponse;
+import com.ai.opt.base.vo.ResponseHeader;
+import com.ai.opt.sdk.components.sequence.util.SeqUtil;
+import com.ai.opt.sdk.util.BeanUtils;
+import com.ai.paas.ipaas.util.StringUtil;
+import com.ai.yc.user.api.usercollectiontranslation.interfaces.IYCUserCollectionSV;
+import com.ai.yc.user.api.usercollectiontranslation.param.UserCollectionInfoRequest;
+import com.ai.yc.user.api.usercollectiontranslation.param.UserCollectionInfoResponse;
+import com.ai.yc.user.dao.mapper.bo.UsrCollectionTranslation;
+import com.ai.yc.user.dao.mapper.bo.UsrCollectionTranslationCriteria;
+import com.ai.yc.user.service.business.interfaces.IYCUserCollectionBusiSV;
+import com.alibaba.dubbo.config.annotation.Service;
+@Service
+@Component
+public class YCUserCollectionSVImpl implements IYCUserCollectionSV{
+
+	@Autowired
+	private IYCUserCollectionBusiSV ycUserCollectionSV;
+	
+	@Override
+	public BaseResponse insertCollectionInfo(
+			UserCollectionInfoRequest userInfoRequest) {
+		BaseResponse response = new BaseResponse();
+		ResponseHeader header = null;
+		String collectId = SeqUtil.getNewId("YC_USER$COLLECTION_ID$SEQ", 8);
+		UsrCollectionTranslation collectionTranslation = new UsrCollectionTranslation();
+		BeanUtils.copyProperties(collectionTranslation, userInfoRequest);
+		collectionTranslation.setCollectionId(collectId);
+		response.setResponseHeader(header);
+		return ycUserCollectionSV.insertCollectionInfo(collectionTranslation);
+	}
+
+	@Override
+	public UserCollectionInfoResponse queryCollectionInfo(
+			UserCollectionInfoRequest userInfoRequest) {
+		UsrCollectionTranslationCriteria example = new UsrCollectionTranslationCriteria();
+		UsrCollectionTranslationCriteria.Criteria criteria = example.createCriteria();
+		if(!StringUtil.isBlank(userInfoRequest.getCollectionId())){
+			criteria.andCollectionIdEqualTo(userInfoRequest.getCollectionId());
+		}
+		return ycUserCollectionSV.queryCollectionInfo(userInfoRequest);
+	}
+
+	@Override
+	public BaseResponse updateCollectionInfo(
+			UserCollectionInfoRequest userInfoRequest) {
+		UsrCollectionTranslation translation = new UsrCollectionTranslation();
+		BeanUtils.copyProperties(translation, userInfoRequest);
+		return ycUserCollectionSV.updateCollectionInfo(translation);
+	}
+
+	@Override
+	public BaseResponse deleteCollectionInfo(List<String> collectionIds) {
+		return ycUserCollectionSV.deleteCollectionInfo(collectionIds);
+	}
+
+}
---
+++ user-core/src/main/java/com/ai/yc/user/api/usercontact/impl/YCUserContactSVImpl.java
@@ -0,0 +1,67 @@
+package com.ai.yc.user.api.usercontact.impl;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.ai.opt.base.vo.BaseResponse;
+import com.ai.opt.sdk.components.sequence.util.SeqUtil;
+import com.ai.opt.sdk.util.BeanUtils;
+import com.ai.yc.user.api.usercontact.interfaces.IYCUserContactSV;
+import com.ai.yc.user.api.usercontact.param.UserContactInfoRequest;
+import com.ai.yc.user.api.usercontact.param.UserContactInfoRespose;
+import com.ai.yc.user.dao.mapper.bo.UsrContact;
+import com.ai.yc.user.service.business.interfaces.IYCUserContactBusiSV;
+import com.alibaba.dubbo.config.annotation.Service;
+@Service
+@Component
+public class YCUserContactSVImpl implements IYCUserContactSV{
+	@Autowired
+	private IYCUserContactBusiSV ycUsrContactBusiSv;
+	@Override
+	@POST
+	@Path("/insertContactInfo")
+	public BaseResponse insertContactInfo(UserContactInfoRequest contactInfo) {
+		UsrContact userContact = new UsrContact();
+		String contactId = SeqUtil.getNewId("YC_USER$CONTACT_ID$SEQ", 8);
+		BeanUtils.copyProperties(userContact, contactInfo);
+		userContact.setContactId(contactId);
+		return ycUsrContactBusiSv.insertContactInfo(userContact);
+	}
+
+	@Override
+	@POST
+	@Path("/updateContactInfo")
+	public BaseResponse updateContactInfo(UserContactInfoRequest contactInfo) {
+		UsrContact userContact = new UsrContact();
+		BeanUtils.copyProperties(userContact, contactInfo);
+		return ycUsrContactBusiSv.updateContactInfo(userContact);
+	}
+
+	@Override
+	@POST
+	@Path("/deleteContactInfo")
+	public BaseResponse deleteContactInfo(String contactId) {
+		return ycUsrContactBusiSv.deleteContactInfo(contactId);
+	}
+
+	@Override
+	@POST
+	@Path("/queryContactInfo")
+	public UserContactInfoRespose queryContactInfo(String userId) {
+		return ycUsrContactBusiSv.queryContactInfo(userId);
+	}
+
+	@Override
+	@POST
+	@Path("/updateContactDefaultMode")
+	public BaseResponse updateContactDefaultMode(
+			UserContactInfoRequest contactInfo) {
+		UsrContact userContact = new UsrContact();
+		BeanUtils.copyProperties(userContact, contactInfo);
+		return ycUsrContactBusiSv.updateContactDefaultMode(userContact);
+	}
+
+}
