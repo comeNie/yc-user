2b11c04ebd2c19d3d475272eb64190516b45b1ca
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2017-02-22 11:44:10 +0800

    添加源语言和目标语言属性

--- user-api/src/main/java/com/ai/yc/user/api/usercollectiontranslation/param/UserCollectionInfoRequest.java
+++ user-api/src/main/java/com/ai/yc/user/api/usercollectiontranslation/param/UserCollectionInfoRequest.java
@@ -22,6 +22,15 @@ public class UserCollectionInfoRequest {
 	 */
 	private String collectTime;
 	
+	/**
+	 * 源语言
+	 */
+	private String sourceLanguage;
+	/**
+	 * 目标语言
+	 */
+	private String targetLanguage;
+	
 	public String getCollectionId() {
 		return collectionId;
 	}
@@ -52,4 +61,17 @@ public class UserCollectionInfoRequest {
 	public void setCollectTime(String collectTime) {
 		this.collectTime = collectTime;
 	}
+	public String getSourceLanguage() {
+		return sourceLanguage;
+	}
+	public void setSourceLanguage(String sourceLanguage) {
+		this.sourceLanguage = sourceLanguage;
+	}
+	public String getTargetLanguage() {
+		return targetLanguage;
+	}
+	public void setTargetLanguage(String targetLanguage) {
+		this.targetLanguage = targetLanguage;
+	}
+	
 }
