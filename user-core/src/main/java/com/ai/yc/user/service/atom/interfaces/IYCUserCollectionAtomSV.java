6369fc3c9babe751afb9017cadb4b1ac474d56a2
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2017-02-22 10:26:01 +0800

    收藏

---
+++ user-core/src/main/java/com/ai/yc/user/service/atom/interfaces/IYCUserCollectionAtomSV.java
@@ -0,0 +1,13 @@
+package com.ai.yc.user.service.atom.interfaces;
+
+import java.util.List;
+
+import com.ai.yc.user.dao.mapper.bo.UsrCollectionTranslation;
+import com.ai.yc.user.dao.mapper.bo.UsrCollectionTranslationCriteria;
+
+public interface IYCUserCollectionAtomSV {
+	public int insertCollectionInfo(UsrCollectionTranslation contactInfo);
+	public int updateCollectionInfo(UsrCollectionTranslation contactInfo);
+	public int deleteCollectionInfo(UsrCollectionTranslationCriteria example);
+	public List<UsrCollectionTranslation> queryCollectionInfo(UsrCollectionTranslationCriteria example);
+}
