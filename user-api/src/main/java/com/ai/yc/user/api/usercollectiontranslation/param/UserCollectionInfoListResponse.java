973c3472819210574982e60c0c1e2971e3083c82
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2017-03-09 11:21:11 +0800

    修改属性名称

--- user-api/src/main/java/com/ai/yc/user/api/usercollectiontranslation/param/UserCollectionInfoListResponse.java
+++ user-api/src/main/java/com/ai/yc/user/api/usercollectiontranslation/param/UserCollectionInfoListResponse.java
@@ -1,6 +1,5 @@
 package com.ai.yc.user.api.usercollectiontranslation.param;
 
-import java.util.List;
 
 import com.ai.opt.base.vo.BaseResponse;
 import com.ai.opt.base.vo.PageInfo;
@@ -9,16 +8,13 @@ public class UserCollectionInfoListResponse extends BaseResponse{
 
 	private static final long serialVersionUID = 1L;
 	
-	public PageInfo<UserCollectionInfo> getCollectionListByPage;
-	
+	public PageInfo<UserCollectionInfo> collectionList;
 
-	public PageInfo<UserCollectionInfo> getGetCollectionListByPage() {
-		return getCollectionListByPage;
+	public PageInfo<UserCollectionInfo> getCollectionList() {
+		return collectionList;
 	}
 
-	public void setGetCollectionListByPage(
-			PageInfo<UserCollectionInfo> getCollectionListByPage) {
-		this.getCollectionListByPage = getCollectionListByPage;
+	public void setCollectionList(PageInfo<UserCollectionInfo> collectionList) {
+		this.collectionList = collectionList;
 	}
-
 }
