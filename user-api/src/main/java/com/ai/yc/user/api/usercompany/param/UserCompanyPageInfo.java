ed96a127d91bcc4ec4a82bec94e549dae57cd6e8
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2017-03-21 18:51:17 +0800

    修改属性

---
+++ user-api/src/main/java/com/ai/yc/user/api/usercompany/param/UserCompanyPageInfo.java
@@ -0,0 +1,86 @@
+package com.ai.yc.user.api.usercompany.param;
+
+import java.sql.Timestamp;
+
+public class UserCompanyPageInfo {
+	/**
+	 * 用户Id
+	 */
+	private String adminUserId;
+	/**
+	 * 用户来源
+	 */
+	private String usersource;
+	/**
+	 * 企业名称
+	 */
+	private String companyName;
+	/**
+	 * 昵称
+	 */
+	private String nickName;
+	/**
+	 * 座机
+	 */
+	private String telephone;
+	/**
+	 * 联系人
+	 */
+	private String linkman;
+	/**
+	 * 创建时间
+	 */
+	private Timestamp createTime;
+	/**
+	 * 审核状态
+	 */
+	private Integer state;
+	public String getAdminUserId() {
+		return adminUserId;
+	}
+	public void setAdminUserId(String adminUserId) {
+		this.adminUserId = adminUserId;
+	}
+	public String getUsersource() {
+		return usersource;
+	}
+	public void setUsersource(String usersource) {
+		this.usersource = usersource;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getLinkman() {
+		return linkman;
+	}
+	public void setLinkman(String linkman) {
+		this.linkman = linkman;
+	}
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
+	public Integer getState() {
+		return state;
+	}
+	public void setState(Integer state) {
+		this.state = state;
+	}
+}
--- user-api/src/main/java/com/ai/yc/user/api/usercompany/param/UserCompanyPageInfoRequest.java
+++ user-api/src/main/java/com/ai/yc/user/api/usercompany/param/UserCompanyPageInfoRequest.java
@@ -2,96 +2,37 @@ package com.ai.yc.user.api.usercompany.param;
 
 import java.sql.Timestamp;
 
+
 public class UserCompanyPageInfoRequest {
 		/**
-		 * 企业编号
+		 * 昵称
 		 */
-	    private String companyId;
-	    /**
-	     * 企业名称
-	     */
-	    private String companyName;
-	    /**
-	     * 营业执照Id
-	     */
-	    private String licenseAttacid;
-	    /**
-	     * 地址
-	     */
-	    private String address;
-	    /**
-	     * 邮件
-	     */
-	    private String companyEmail;
-	    /**
-	     * 联系电话(座机)
-	     */
-	    private String telephone;
-	    /**
-	     * 联系人
-	     */
-	    private String linkman;
-	    /**
-	     * 手机号
-	     */
-	    private String mobilephone;
-	    /**
-	     * 企业logo
-	     */
-	    private String entpAttacid;
-	    /**
-	     * 企业简介
-	     */
-	    private String content;
-	    /**
-	     * 申请人Id
-	     */
-	    private String proposerId;
-
-	    /**
-	     * 申请名称
-	     */
-	    private String proposerName;
-	    /**
-	     * 启用状态
-	     */
-	    private Integer state;
-	    /**
-	     * 创建时间
-	     */
-	    private Timestamp createTime;
-	    /**
-	     * 修改时间
-	     */
-	    private Timestamp modifyTime;
-	    /**
-	     * 备注
-	     */
-	    private String remark;
-	    /**
-	     * 是否是初始化数据
-	     */
-	    private Integer isInitalData;
-	    /**
-	     * 账户Id
-	     */
-	    private Long accountId;
-	    /**
-	     * 管理员用户Id
-	     */
-	    private String adminUserId;
-	    /**
-	     * 国家
-	     */
-	    private String country;
-	    /**
-	     * 省
-	     */
-	    private String province;
-	    /**
-	     * 市
-	     */
-	    private String cnCity;
+		private String nickName;
+		/**
+		 * 手机
+		 */
+		private String moblePhone;
+		/**
+		 * 企业名称
+		 */
+		private String companyName;
+		/**
+		 * 审核人
+		 */
+		private String checkName;
+		/**
+		 * 申请来源
+		 */
+		private String applicationSource;
+		/**
+		 * 创建时间
+		 */
+		private Timestamp createTime;
+		/**
+		 * 审核状态
+		 */
+		private Integer state;
+	
 	    /**
 	     * 当前第几页,必填
 	     */
@@ -102,182 +43,6 @@ public class UserCompanyPageInfoRequest {
 	     */
 	    private Integer pageSize;
 		
-
-	    public String getCompanyId() {
-	        return companyId;
-	    }
-
-	    public void setCompanyId(String companyId) {
-	        this.companyId = companyId == null ? null : companyId.trim();
-	    }
-
-	    public String getCompanyName() {
-	        return companyName;
-	    }
-
-	    public void setCompanyName(String companyName) {
-	        this.companyName = companyName == null ? null : companyName.trim();
-	    }
-
-	    public String getLicenseAttacid() {
-	        return licenseAttacid;
-	    }
-
-	    public void setLicenseAttacid(String licenseAttacid) {
-	        this.licenseAttacid = licenseAttacid == null ? null : licenseAttacid.trim();
-	    }
-
-	    public String getAddress() {
-	        return address;
-	    }
-
-	    public void setAddress(String address) {
-	        this.address = address == null ? null : address.trim();
-	    }
-
-	    public String getCompanyEmail() {
-	        return companyEmail;
-	    }
-
-	    public void setCompanyEmail(String companyEmail) {
-	        this.companyEmail = companyEmail == null ? null : companyEmail.trim();
-	    }
-
-	    public String getTelephone() {
-	        return telephone;
-	    }
-
-	    public void setTelephone(String telephone) {
-	        this.telephone = telephone == null ? null : telephone.trim();
-	    }
-
-	    public String getLinkman() {
-	        return linkman;
-	    }
-
-	    public void setLinkman(String linkman) {
-	        this.linkman = linkman == null ? null : linkman.trim();
-	    }
-
-	    public String getMobilephone() {
-	        return mobilephone;
-	    }
-
-	    public void setMobilephone(String mobilephone) {
-	        this.mobilephone = mobilephone == null ? null : mobilephone.trim();
-	    }
-
-	    public String getEntpAttacid() {
-	        return entpAttacid;
-	    }
-
-	    public void setEntpAttacid(String entpAttacid) {
-	        this.entpAttacid = entpAttacid == null ? null : entpAttacid.trim();
-	    }
-
-	    public String getContent() {
-	        return content;
-	    }
-
-	    public void setContent(String content) {
-	        this.content = content == null ? null : content.trim();
-	    }
-
-	    public String getProposerId() {
-	        return proposerId;
-	    }
-
-	    public void setProposerId(String proposerId) {
-	        this.proposerId = proposerId == null ? null : proposerId.trim();
-	    }
-
-	    public String getProposerName() {
-	        return proposerName;
-	    }
-
-	    public void setProposerName(String proposerName) {
-	        this.proposerName = proposerName == null ? null : proposerName.trim();
-	    }
-
-	    public Integer getState() {
-	        return state;
-	    }
-
-	    public void setState(Integer state) {
-	        this.state = state;
-	    }
-
-	    public Timestamp getCreateTime() {
-	        return createTime;
-	    }
-
-	    public void setCreateTime(Timestamp createTime) {
-	        this.createTime = createTime;
-	    }
-
-	    public Timestamp getModifyTime() {
-	        return modifyTime;
-	    }
-
-	    public void setModifyTime(Timestamp modifyTime) {
-	        this.modifyTime = modifyTime;
-	    }
-
-	    public String getRemark() {
-	        return remark;
-	    }
-
-	    public void setRemark(String remark) {
-	        this.remark = remark == null ? null : remark.trim();
-	    }
-
-	    public Integer getIsInitalData() {
-	        return isInitalData;
-	    }
-
-	    public void setIsInitalData(Integer isInitalData) {
-	        this.isInitalData = isInitalData;
-	    }
-
-	    public Long getAccountId() {
-	        return accountId;
-	    }
-
-	    public void setAccountId(Long accountId) {
-	        this.accountId = accountId;
-	    }
-
-	    public String getAdminUserId() {
-	        return adminUserId;
-	    }
-
-	    public void setAdminUserId(String adminUserId) {
-	        this.adminUserId = adminUserId == null ? null : adminUserId.trim();
-	    }
-
-		public String getCountry() {
-			return country;
-		}
-
-		public void setCountry(String country) {
-			this.country = country;
-		}
-
-		public String getProvince() {
-			return province;
-		}
-
-		public void setProvince(String province) {
-			this.province = province;
-		}
-
-		public String getCnCity() {
-			return cnCity;
-		}
-
-		public void setCnCity(String cnCity) {
-			this.cnCity = cnCity;
-		}
 
 		public Integer getPageNo() {
 			return pageNo;
@@ -293,6 +58,62 @@ public class UserCompanyPageInfoRequest {
 
 		public void setPageSize(Integer pageSize) {
 			this.pageSize = pageSize;
+		}
+
+		public String getNickName() {
+			return nickName;
+		}
+
+		public void setNickName(String nickName) {
+			this.nickName = nickName;
+		}
+
+		public String getMoblePhone() {
+			return moblePhone;
+		}
+
+		public void setMoblePhone(String moblePhone) {
+			this.moblePhone = moblePhone;
+		}
+
+		public String getCompanyName() {
+			return companyName;
+		}
+
+		public void setCompanyName(String companyName) {
+			this.companyName = companyName;
+		}
+
+		public String getCheckName() {
+			return checkName;
+		}
+
+		public void setCheckName(String checkName) {
+			this.checkName = checkName;
+		}
+
+		public String getApplicationSource() {
+			return applicationSource;
+		}
+
+		public void setApplicationSource(String applicationSource) {
+			this.applicationSource = applicationSource;
+		}
+
+		public Timestamp getCreateTime() {
+			return createTime;
+		}
+
+		public void setCreateTime(Timestamp createTime) {
+			this.createTime = createTime;
+		}
+
+		public Integer getState() {
+			return state;
+		}
+
+		public void setState(Integer state) {
+			this.state = state;
 		}
 	    
 }
