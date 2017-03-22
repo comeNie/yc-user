3b8aab90ab950a41b659566ca53145c2b37898ee
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2017-03-22 15:23:46 +0800

    修改sql

--- user-core/src/main/java/com/ai/yc/user/dao/mapper/attach/UsrCompanyQueryList.java
+++ user-core/src/main/java/com/ai/yc/user/dao/mapper/attach/UsrCompanyQueryList.java
@@ -1,5 +1,6 @@
 package com.ai.yc.user.dao.mapper.attach;
 
+import com.ai.paas.ipaas.util.StringUtil;
 import com.ai.yc.user.api.usercompany.param.UserCompanyPageInfoRequest;
 
 public class UsrCompanyQueryList {
@@ -25,6 +26,10 @@ public class UsrCompanyQueryList {
 		if(pageInfoRequest.getCheckName()!=null&&!"".equals(pageInfoRequest.getCheckName())){
 			stringBuffer.append("company.auditor =" +pageInfoRequest.getCheckName() +" and ") ;
 		}
+		if(!StringUtil.isBlank(pageInfoRequest.getUserSource())){
+			stringBuffer.append("usr.usersource =" +pageInfoRequest.getUserSource() +" and ");
+		}
+
 		stringBuffer.append(" 1 = 1") ;
 		int limitStart = (pageInfoRequest.getPageNo()-1)*pageInfoRequest.getPageSize();
 		int limitEnd = pageInfoRequest.getPageSize();
@@ -34,7 +39,7 @@ public class UsrCompanyQueryList {
 	
 	public String queryUserCompanyCount(UserCompanyPageInfoRequest pageInfoRequest){
 		StringBuffer stringBuffer = new StringBuffer();
-		stringBuffer.append("select count(*) a from usr_user usr,usr_company company where usr.user_id = company.admin_user_id and ");
+		stringBuffer.append("select count(*) count from usr_user usr,usr_company company where usr.user_id = company.admin_user_id and ");
 		/**
 		 * 待审核企业
 		 */
@@ -52,6 +57,9 @@ public class UsrCompanyQueryList {
 		}
 		if(pageInfoRequest.getCheckName()!=null&&!"".equals(pageInfoRequest.getCheckName())){
 			stringBuffer.append("company.auditor =" +pageInfoRequest.getCheckName() +" and ") ;
+		}
+		if(!StringUtil.isBlank(pageInfoRequest.getUserSource())){
+			stringBuffer.append("usr.usersource =" +pageInfoRequest.getUserSource() +" and ");
 		}
 		stringBuffer.append(" 1 = 1") ;
 		
